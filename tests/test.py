f1293bb95248091340f9f8a7d4ae3657a9007f53
Asuka Minato <i@asukaminato.eu.org>
2024-10-10 06:09:58 +0900

    update (#2)

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,12 @@
 import datetime
 import rs_fsrs_python
-from rs_fsrs_python import Rating
-c = rs_fsrs_python.Card()
-f = rs_fsrs_python.FSRS()
-t = datetime.datetime.now(tz=datetime.timezone.utc)
-t = t.replace(tzinfo=datetime.timezone.utc)
-scheduled_cards = f.schedule(c, t)
-updated_card = scheduled_cards.select_card(Rating.Easy)
-print(updated_card.log())
+from rs_fsrs_python import Rating, Card, FSRS, RecordLog
+
+f = FSRS.default()
+c = Card()
+
+scheduled_cards = f.repeat(c, datetime.datetime.now(tz=datetime.timezone.utc))
+for rating in [Rating.Again, Rating.Hard, Rating.Good, Rating.Easy]:
+    updated_card = scheduled_cards.get(rating)
+    print(updated_card.card)
+    print(updated_card.review_log)
